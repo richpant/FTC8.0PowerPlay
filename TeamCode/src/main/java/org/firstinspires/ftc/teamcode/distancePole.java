202b53adf8b52379ba833963d9fac2165ae92487
richpant <47926579+richpant@users.noreply.github.com>
2022-11-23 15:04:09 -0600

    Distance sensor?

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/distancePole.java
@@ -0,0 +1,245 @@
+/*
+Copyright (c) 2018 FIRST
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to the limitations in the disclaimer below) provided that
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Neither the name of FIRST nor the names of its contributors may be used to
+endorse or promote products derived from this software without specific prior
+written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+/**
+ * {@link distancePole} illustrates how to use the REV Robotics
+ * Time-of-Flight Range Sensor.
+ *
+ * The op mode assumes that the range sensor is configured with a name of "sensor_range".
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ *
+ * @see <a href="http://revrobotics.com">REV Robotics Web Page</a>
+ */
+@TeleOp(name = "DistancePole", group = "Sensor")
+@Disabled
+public class distancePole extends LinearOpMode {
+
+    private DistanceSensor leftRange;
+    private DistanceSensor rightRange;
+    private DistanceSensor middleRange;
+    private DcMotor rightFront;
+    private DcMotor rightRear;
+    private DcMotor leftFront;
+    private DcMotor leftRear;
+    private DcMotor lift;
+    private Servo claw;
+    private Servo claw2;
+    private BNO055IMU imu         = null;
+    double open = 0.36;
+    double close = 0.5;
+    @Override
+    public void runOpMode() {
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        leftRear = hardwareMap.get(DcMotor.class, "leftRear");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        rightRear = hardwareMap.get(DcMotor.class, "rightRear");
+        lift = hardwareMap.get(DcMotor.class,"lift");
+        claw = hardwareMap.get(Servo.class,"claw");
+        claw = hardwareMap.get(Servo.class, "claw2");
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit            = BNO055IMU.AngleUnit.DEGREES;
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightRear.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+        // Ensure the robot is stationary.  Reset the encoders and set the motors to BRAKE mode
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightRear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        // you can use this as a regular DistanceSensor.
+        leftRange = hardwareMap.get(DistanceSensor.class, "leftrange");
+        rightRange = hardwareMap.get(DistanceSensor.class, "rightrange");
+        middleRange = hardwareMap.get(DistanceSensor.class, "middlerange");
+        // you can also cast this to a Rev2mDistanceSensor if you want to use added
+        // methods associated with the Rev2mDistanceSensor class.
+        Rev2mDistanceSensor sensorTimeOfFlight1 = (Rev2mDistanceSensor)leftRange;
+        Rev2mDistanceSensor sensorTimeOfFlight2 = (Rev2mDistanceSensor)rightRange;
+        Rev2mDistanceSensor sensorTimeOfFlight3 = (Rev2mDistanceSensor)middleRange;
+        telemetry.addData(">>", "Press start to continue");
+        telemetry.update();
+
+        waitForStart();  claws(close);
+
+
+        while(opModeIsActive()) {
+            liftup(500);
+            move(1700,-1700,-1700,1700);
+            if(middleRange.getDistance(DistanceUnit.INCH)<12.00){
+                move(0,0,0,0);
+            }
+            moveToPole(6);
+
+
+            // generic DistanceSensor methods. for left
+            telemetry.addData("deviceName",leftRange.getDeviceName() );
+            telemetry.addData("range", String.format("%.01f in", leftRange.getDistance(DistanceUnit.INCH)));
+            // Rev2mDistanceSensor specific methods.
+            telemetry.addData("ID", String.format("%x", sensorTimeOfFlight1.getModelID()));
+            telemetry.addData("did time out", Boolean.toString(sensorTimeOfFlight1.didTimeoutOccur()));
+
+            //right
+            telemetry.addData("deviceName",rightRange.getDeviceName() );
+            telemetry.addData("range", String.format("%.01f in", rightRange.getDistance(DistanceUnit.INCH)));
+            // Rev2mDistanceSensor specific methods.
+            telemetry.addData("ID", String.format("%x", sensorTimeOfFlight2.getModelID()));
+            telemetry.addData("did time out", Boolean.toString(sensorTimeOfFlight2.didTimeoutOccur()));
+
+            //middle
+            telemetry.addData("deviceName",middleRange.getDeviceName() );
+            telemetry.addData("range", String.format("%.01f in", middleRange.getDistance(DistanceUnit.INCH)));
+            // Rev2mDistanceSensor specific methods.
+            telemetry.addData("ID", String.format("%x", sensorTimeOfFlight3.getModelID()));
+            telemetry.addData("did time out", Boolean.toString(sensorTimeOfFlight3.didTimeoutOccur()));
+            telemetry.update();
+        }
+    }
+
+    public void claws(double q) {
+        claw.setPosition(1 - q);
+        claw2.setPosition(q);
+    }
+
+    public void move(int lf, int lb, int rf, int rb) {
+        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        rightFront.setTargetPosition(rf);
+        rightRear.setTargetPosition(rb);
+        leftFront.setTargetPosition(lf);
+        leftRear.setTargetPosition(lb);
+
+        rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightRear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftRear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        rightRear.setPower(0.5);
+        rightFront.setPower(0.5);
+
+        leftFront.setPower(0.5);
+        leftRear.setPower(0.5);
+
+        while (leftFront.isBusy() && leftRear.isBusy() && rightFront.isBusy() && rightRear.isBusy()) {
+            sleep(25);
+            if(middleRange.getDistance(DistanceUnit.INCH)<12.00){  //or whatever distance
+                move(0,0,0,0);
+            }
+        }
+        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        rightRear.setPower(0);
+        leftFront.setPower(0);
+        rightFront.setPower(0);
+        leftRear.setPower(0);
+    }
+
+    //===============================lift====================
+    //----------------------Lift------------
+    public void liftup(int encod) {
+        //lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        lift.setTargetPosition(encod);
+
+        lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        lift.setPower(1);
+        while (lift.isBusy()) {
+            sleep(50);
+        }
+        lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        lift.setPower(0);
+
+    }
+
+    public void liftDown(int down) {
+
+        lift.setTargetPosition(down);
+
+        lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        lift.setPower(-1);
+        while (lift.isBusy()) {
+            sleep(50);
+        }
+        lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        lift.setPower(0);
+
+    }
+    public void moveToPole(double pole){
+        if(middleRange.getDistance(DistanceUnit.INCH)<pole){
+            leftFront.setPower(.3);
+            leftRear.setPower(.3);
+            rightFront.setPower(.3);
+            rightRear.setPower(.3);
+        }else{
+            leftFront.setPower(0);
+            leftRear.setPower(0);
+            rightFront.setPower(0);
+            rightRear.setPower(0);
+        }
+    }
+
+}
